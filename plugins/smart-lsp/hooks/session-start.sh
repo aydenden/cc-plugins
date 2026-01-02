32c33af53301bd3e2afb02ca715f3edc817b20f5
aydenden <lucky72nyh@gmail.com>
2026-01-03 01:34:59 +0900

    feat: lsp

---
+++ plugins/smart-lsp/hooks/session-start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo '{"hookSpecificOutput":{"hookEventName":"SessionStart","additionalContext":"Tip: Prefer LSP tools (goToDefinition, hover, documentSymbol) over Grep for symbol search"}}'
